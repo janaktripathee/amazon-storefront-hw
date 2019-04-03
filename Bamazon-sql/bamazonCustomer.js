40702869124bac76e7f4eec0cc939ac62350e942
janak <janaktripathee@gmail.com>
2019-04-03 16:38:42 -0700

    added full functionality

--- Bamazon-sql/bamazonCustomer.js
+++ Bamazon-sql/bamazonCustomer.js
@@ -1,136 +1,95 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
-var Table = require('cli-table');
+require('console.table');
 
 
 var connection = mysql.createConnection({
-	host:"localhost",
-	user:'root',
-	password:"password",
-	database:"bamazon_db",
-	port:3306
-	
+    host: "localhost",
+    user: 'root',
+    password: "password",
+    database: "bamazon_db",
+    port: 3306
+
 });
 
 
 
-connection.connect(function(err){
-	if(err){
-		console.log("error connecting:" + err.stack);
-		return;
-	}
-	displayProduct();
+connection.connect(function(err) {
+    if (err) {
+        console.log("error connecting:" + err.stack);
+        return;
+    }
+    displayProduct();
 
 })
 
-function displayProduct(){
-	connection.query("SELECT * FROM products", function(err, res){
-		if(err) throw err;
+function displayProduct() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
         console.table(res);
-		promptCustomerItem(res);
+        promptCustomerItem(res);
 
-	})
+    })
 }
 
-function promptCustomerItem(item){
-inquirer
-	.prompt([
-	{
-		type:"input",
-		name:'choice',
-		message: "what is the product id that you want to buy?",
-		validate:function(val){
-			return !isNaN(val) || val.toLowerCase() === 'q';
-		}
+function promptCustomerItem(item) {
+    inquirer
+        .prompt([{
+            type: "input",
+            name: 'choice',
+            message: "what is the product id that you want to buy?"
 
-	}
-	]).then(function(val){
-		checkExit(val.choice);
-		var choiceId= parseInt(val.choice);
-		var product = checkProductIfExists(choiceId, item);
-    //console.log(product);
-
-		if(product){
-			promptCustomerQuantity(product);
-		}
-		else{
-			console.log('\nThis item is out of stock.');
-			displayProduct();
-		}
-	});
-}
-		
-		
-
-function promptCustomerQuantity(product){
-inquirer
-	.prompt([
-	{
-		type:"input",
-		name:'quantity',
-		message: "How many units of the product you want to buy",
-		validate:function(val){
-			return val>0 || val.toLowerCase() === 'q';
-		}
-
-	}
-	]).then(function(inquirerResponse){
-    
-		//console.log(inquirerResponse,"responseofwar")
-		checkExit(inquirerResponse.stock_quality);
-		var productQuantity= parseInt(inquirerResponse.stock_quality);
-   console.log(productQuantity,"productquantity");
-   console.log(product.stock_quality,"productstock");
+        }]).then(function(val) {
+       
+            //checkExit(val.choice);
+            var choiceId = parseInt(val.choice);
+            var product = checkProductIfExists(choiceId, item);
 
 
-		if(productQuantity > product.stock_quality){
-			console.log('\nThis item is out of stock.');
-			displayProduct();
-		}
-		else{
-
-			purchase(product, quantity);
-		}
-	});
+            if (product) {
+                promptCustomerQuantity(product);
+            } else {
+                console.log('\nThis item is not present at the moment, please check back later.');
+                displayProduct();
+            }
+        });
 }
 
 
-function purchase(product, quantity){
-    console.log(quantity, "quantity")
-	connection.query('UPDATE products SET stock_quantity = stock_quantity - ? WHERE id = ?',
-		[quantity, product.id],
-		function(err, res){
-			console.log("\n you have succefully purchased this "+ product.product_name +"product with quantity" + quantity);
-			displayProduct();
-		})
-};
 
-function checkProductIfExists(choiceId, item){
-	for(var i=0; i< item.length ;i++){
-		if(item[i].id === choiceId){
-			return item[i];
-		}
-	}
-	return null;
+function promptCustomerQuantity(product) {
+    inquirer
+        .prompt([{
+            type: "input",
+            name: "quantity",
+            message: "How many units of the product you want to buy"
 
-
-}
-
-
-function checkExit(choice){
-    console.log(choice,"choice");
-	if(choice.toLowerCase()=== 'q'){
-		console.log('succefully logged out');
-		process.exit(0);
-	}
+        }])
+        .then(function(val) {
+            //checkExit(val.quantity);
+            var productQuantity = parseInt(val.quantity);
+            if (productQuantity > product.stock_quantity) {
+                console.log('\nThis item is out of stock.');
+                displayProduct();
+            } else {
+                connection.query("UPDATE products SET stock_quantity = stock_quantity - ? WHERE id = ?",
+                    [val.name, val.id],
+                    function(err, res) {
+                        console.log("\n you have succefully purchased this " + product.product_name + " product with quantity " + val.quantity);
+                        displayProduct();
+                    })
+            }
+        });
 }
 
 
 
 
-
-
-
-
-
-
+function checkProductIfExists(choiceId, item) {
+    for (var i = 0; i < item.length; i++) {
+        if (item[i].id === choiceId) {
+            return item[i];
+        }
+    }
+    return null;
+}
--- Bamazon-sql/bamazon_db.sql
+++ Bamazon-sql/bamazon_db.sql
@@ -10,20 +10,20 @@ CREATE TABLE products(
 	product_name VARCHAR(255) NOT NULL,
 	department_name VARCHAR(255) NOT NULL,
 	price INT NOT NULL,
-	stock_quality INT NOT NULL,
+	stock_quantity INT NOT NULL,
 	PRIMARY KEY(id)
 
 );
 
-INSERT INTO products(product_name, department_name, price, stock_quality) VALUES ('Apple Iphone XR','Best Buy','800','5'),
-('iPad Air','Frys','249','3'),
-('Samsung-Galaxy Note8','Walmart','549','8'),
-('Pizza Oven','Costco','55','9'),
-('AVG Internet Security','Best Buy','10','30'),
-('Geometric Comforter Set','Target','44','20'),
-('Macbook air','Apple Store','1400','3'),
-('Pull-over Hoodie','Kohls','13','0'),
-('Google Home mini','Best Buy','49','3'),
-('Google home voice-activated speaker','walmart','99','2');
+INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('Apple Iphone XR','Best Buy',800,5),
+('iPad Air','Frys',249,3),
+('Samsung-Galaxy Note8','Walmart',549,8),
+('Pizza Oven','Costco',55,9),
+('AVG Internet Security','Best Buy',10,30),
+('Geometric Comforter Set','Target',44,20),
+('Macbook air','Apple Store',1400,3),
+('Pull-over Hoodie','Kohls',13,0),
+('Google Home mini','Best Buy',49,3),
+('Google home voice-activated speaker','walmart',99,2);
 
 SELECT * FROM products;
